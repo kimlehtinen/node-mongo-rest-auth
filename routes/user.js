96267ab8b5135bd1955e09efe53c3a03d9eddcd3
kim3z <kim.lehtinen@hotmail.com>
2018-10-13 15:16:45 +0200

    Added docker-compose.yml. Fixed some linting

--- routes/user.js
+++ routes/user.js
@@ -1,58 +1,55 @@
-const express = require("express");
-const mongoose = require("mongoose");
-const router = express.Router();
-const bcrypt = require("bcrypt");
-const User = require("../models/user");
+const express = require('express');
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
+const User = require('../models/user');
 
-router.post("/signup", (req, res, next) => {
+const router = express.Router();
+
+router.post('/signup', (req, res) => {
   User.find({ email: req.body.email })
     .exec()
-    .then(user => {
+    .then((user) => {
       if (user.length >= 1) {
         return res.status(409).json({
-          message: "Email already exists"
-        });
-      } else {
-        bcrypt.hash(req.body.password, 10, (err, hash) => {
-          if (err) {
-            return res.status(500).json({
-              error: err
-            });
-          } else {
-            const newUser = new User({
-              _id: new mongoose.Types.ObjectId(),
-              name: req.body.name,
-              email: req.body.email,
-              password: hash
-            });
-            newUser.save().then(result => {
-                console.log('New user result: ', result);
-                res.status(201).json({
-                  message: "New user created successfully"
-                });
-            }).catch(err => {
-                console.log('New user error: ', err);
-                res.status(500).json({
-                  error: err
-                });
-            });
-          }
+          message: 'Email already exists',
         });
       }
+      bcrypt.hash(req.body.password, 10, (err, hash) => {
+        if (err) {
+          return res.status(500).json({
+            error: err,
+          });
+        }
+        const newUser = new User({
+          _id: new mongoose.Types.ObjectId(),
+          name: req.body.name,
+          email: req.body.email,
+          password: hash,
+        });
+        newUser.save().then(() => {
+          res.status(201).json({
+            message: 'New user created successfully',
+          });
+        }).catch((newUserError) => {
+          res.status(500).json({
+            error: newUserError,
+          });
+        });
+        return false;
+      });
+      return false;
     });
 });
 
-router.delete("/:user_id", (req, res, next) => {
-  User.remove({ _id: req.params.user_id }).exec().then(result => {
-      console.log('Delete user result: ', result);
-      res.status(200).json({
-        message: "User was successfully deleted"
-      });
-    })
-    .catch(err => {
-      console.log('Delete user error: ', err);
+router.delete('/:user_id', (req, res) => {
+  User.remove({ _id: req.params.user_id }).exec().then(() => {
+    res.status(200).json({
+      message: 'User was successfully deleted',
+    });
+  })
+    .catch((err) => {
       res.status(500).json({
-        error: err
+        error: err,
       });
     });
 });
--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
+
 const app = express();
 const http = require('http');
-const morgan = require("morgan");
-const bodyParser = require("body-parser");
-const mongoose = require("mongoose");
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
 const userRoutes = require('./routes/user');
 
 const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 
 mongoose.connect(
-    'mongodb://localhost:27017/example-app', { useCreateIndex: true, useNewUrlParser: true }
+  'mongodb://localhost:27017/example-app', { useCreateIndex: true, useNewUrlParser: true },
 );
 mongoose.Promise = global.Promise;
 
@@ -20,37 +21,38 @@ app.use(morgan('dev'));
 // urlencoded and json
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
-  
+
 app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header(
-      'Access-Control-Allow-Headers',
-      'Origin, X-Requested-With, Content-Type, Accept, Authorization'
-    );
-    if (req.method === 'OPTIONS') {
-      res.header('Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET');
-      return res.status(200).json({});
-    }
-    next();
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  );
+  if (req.method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods", "PUT, POST, PATCH, DELETE, GET');
+    return res.status(200).json({});
+  }
+  next();
+  return false;
 });
-  
+
 // API routes
-app.use("/user", userRoutes);
-  
+app.use('/user', userRoutes);
+
 app.use((req, res, next) => {
-    const error = new Error('Not found');
-    error.status = 404;
-    next(error);
+  const error = new Error('Not found');
+  error.status = 404;
+  next(error);
 });
 
 // Error handling
-app.use((error, req, res, next) => {
-    res.status(error.status || 500);
-    res.json({
-      error: {
-        message: error.message
-      }
-    });
+app.use((error, req, res) => {
+  res.status(error.status || 500);
+  res.json({
+    error: {
+      message: error.message,
+    },
+  });
 });
 
 server.listen(port);
